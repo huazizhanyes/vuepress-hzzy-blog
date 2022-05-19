fa3dc7cd032315061ce7f3415557b72df2a667aa
huazizhanyes <2585717148@qq.com>
2022-05-19 14:24:44 +0800

    add page

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,18 @@
 # npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
-
+# cd docs/.vuepress/dist
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=blog.hzzy.xyz&token=XDBAanwPu0JphsAQ"
 # deploy to github pages
 # echo 'blog.hzzy.xyz' > CNAME
-git add .
-git commit -m 'commit'
+# git add .
+# git commit -m 'commit'
 # git config --global user.name "huazizhanyes"
 # git config --global user.email "2585717148@qq.com"
-git config --global http.sslVerify 'false'
+# git config --global http.sslVerify 'false'
 
-git remote add origin https://github.com/huazizhanyes/hzzy.github.io.git
-git push -u origin main
+# git remote add origin https://github.com/huazizhanyes/hzzy.github.io.git
+# git push -u origin main
 
-cd -
+# cd -
 # rm -rf docs/.vuepress/dist
